434cd4f4b82a33b0f6cc9d1641874c8d4ff80243
maina george <98666205+maina68@users.noreply.github.com>
2024-07-12 06:17:12 +0000

    chore: Add Word Masters game HTML, CSS, and JavaScript files

---
+++ wordle-masters.js
@@ -0,0 +1,46 @@
+const letters = document.querySelectorAll('.scoreboard-letter');
+const loadingDiv = document.querySelector('.info-bar');
+const ANSWER_LENGTH = 5;
+
+async function init() {
+  let currentGuess = '';
+  
+  function addLetter (letter) {
+    if (currentGuess.length < ANSWER_LENGTH) {
+      // add letter to the end
+      currentGuess += letter;
+    } else {
+      // replace the last letter
+      currentGuess = currentGuess.substring(0, currentGuess.length - 1) + letter;
+    }
+    
+    letters[currentGuess.length - 1].innerText = letter;
+  }
+  
+  async function commit() {
+    if (currentGuess.length !== ANSWER_LENGTH) {
+
+    }
+  }
+  
+  document.addEventListener('keydown', function handleKeyPress(event) {
+    const action = event.key;
+    
+    if (action === 'Enter') {
+      commit();
+    } else if (action === 'Backspace') {
+      backspace();
+    } else if (isLetter(action)) {
+      addLetter(action.toUpperCase())
+    } else {
+      // do nothing
+    }
+  });
+}
+
+function isLetter(letter) {
+  return /^[a-zA-Z]$/.test(letter);
+}
+
+
+init();
