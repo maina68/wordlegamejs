2159b5b467e7d23073131ddb4a46dda7f007c5c1
maina george <98666205+maina68@users.noreply.github.com>
2024-07-12 06:34:56 +0000

    feat: Add functionality to add letters to the current row in Word Masters game and backspace

--- wordle-masters.js
+++ wordle-masters.js
@@ -4,7 +4,7 @@ const ANSWER_LENGTH = 5;
 
 async function init() {
   let currentGuess = '';
-  
+  let currentRow = 0;  
   function addLetter (letter) {
     if (currentGuess.length < ANSWER_LENGTH) {
       // add letter to the end
@@ -14,13 +14,29 @@ async function init() {
       currentGuess = currentGuess.substring(0, currentGuess.length - 1) + letter;
     }
     
-    letters[currentGuess.length - 1].innerText = letter;
+    letters[ANSWER_LENGTH * currentRow + currentGuess.length - 1].innerText = letter;
   }
   
   async function commit() {
     if (currentGuess.length !== ANSWER_LENGTH) {
-
+        // do nothing
+        return;
     }
+
+    // TODO validate the word
+
+    //  TODO do all the marking as "correct" or "wrong"
+
+    //TODO did they win or not
+
+    currentRow++;
+    currentGuess = '';
+
+  }
+
+  function backspace() {
+    currentGuess = currentGuess.substring(0, currentGuess.length - 1);
+    letters[ANSWER_LENGTH * currentRow + currentGuess.length].innerText = '';
   }
   
   document.addEventListener('keydown', function handleKeyPress(event) {
